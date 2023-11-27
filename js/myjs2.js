c763f60328536f48e03d8a1922cdbd99e9c3337d
Mauricio Castelao <145405010+maeciosso@users.noreply.github.com>
2023-11-27 20:08:32 -0300

    Add files via upload

---
+++ js/myjs2.js
@@ -0,0 +1,41 @@
+alert("site feito para celulares")
+const $btn = document.getElementById("btn")
+let historicoNotas = []
+
+$btn.addEventListener('click', function(){
+    const nota1 = parseFloat(document.getElementById("1").value);
+    const nota2 = parseFloat(document.getElementById("2").value);
+    const nota3 = parseFloat(document.getElementById("3").value);
+	const nota4 = parseFloat(document.getElementById("4").value);
+    
+    let notaFinal = nota1 + nota2 + nota3 + nota4;
+
+    historicoNotas.unshift(notaFinal.toFixed(2))
+
+        if (historicoNotas.length > 10) {
+            historicoNotas.pop()
+        }
+
+        nota1.value = " ";
+        nota2.value = " ";
+        nota3.value = " ";
+        nota4.value = " ";
+
+        atualizarNotas()
+});
+
+function atualizarNotas(){
+    let $ul = document.getElementById('ul');
+    $ul.innerHTML = ''; 
+    historicoNotas.forEach(function (notaFinal) {
+        let $li = document.createElement('li');
+
+        let resultado = "";
+        if(notaFinal){
+            resultado = "media anual de " + (notaFinal / 4) + " ponto(s)";
+        }
+
+        $li.innerText = resultado
+        $ul.appendChild($li);
+    })
+}
