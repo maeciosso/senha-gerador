6edb72b80bf28ab87d2145cf6151027016fd48a8
Mauricio Castelao <145405010+maeciosso@users.noreply.github.com>
2023-11-28 08:54:42 -0300

    Add files via upload

---
+++ css/index.js
@@ -0,0 +1,25 @@
+const $btn = document.getElementById("btn");
+const menu = document.getElementById("menu")
+const divMenu = document.getElementById("div")
+
+$btn.addEventListener("click", () => {
+        let length = 8;
+        let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        let symbol = "@#$&+*:;!?£¢€¥^=%§"
+        let retVal = [];
+        for (var i = 0, n = charset.length; i < length; ++i) {
+            retVal += (charset.charAt(Math.floor(Math.random() * n)) + symbol.charAt(Math.floor(Math.random() * n)))
+        } if(retVal < length){
+            retVal.pop()
+        }
+
+    let $ul = document.querySelector("ul")
+    $ul.innerHTML = '';
+    let $li = document.createElement('li');
+    $li.innerText = retVal
+    $ul.appendChild($li);
+})
+
+menu.addEventListener("click", () => {
+    divMenu.classList.toggle("hide")
+})
